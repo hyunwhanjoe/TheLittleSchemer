900938afa8ab3e74e5ea942ca27aed0631e872a7
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-06-20 18:00:36 +0900

    chapter 1.rkt - add pg 5

--- chapter 1.rkt
+++ chapter 1.rkt
@@ -5,13 +5,13 @@
     (and (not (pair? x)) (not (null? x)))))
 
 ;pg 3
-;(atom? 'atom)
-;(atom? 'turkey)
-;(atom? 1492)
-;(atom? 'u)
-;(atom? '*abc$)
-;(pair? '(atom)) #t
-;(pair? '(atom turkey or)) #t
+;(atom? 'atom) ;#t
+;(atom? 'turkey) ;#t
+;(atom? 1492) ;#t
+;(atom? 'u) ;#t
+;(atom? '*abc$) ;#t
+;'(atom) ; yes
+;'(atom turkey or) ; yes
 
 ;pg 4
 ;'(atom turkey) or ; no
@@ -22,4 +22,13 @@
 ;'(how are you doing so far) ; yes
 ;'(how are you doing so far) ; 6
 ;'(((how) are) ((you)(doing so)) far) ; yes
-'(((how) are) ((you)(doing so)) far) ; 3
+;'(((how) are) ((you)(doing so)) far) ; 3
+
+;pg 5
+;'() ; yes
+;(atom? '()) ;#f
+;'(()()()()) ;yes
+;(car '(a b c)) ;a
+;(car '((a b c) x y z)) ;(a b c)
+;(car 'hotdog) ; no answer
+;(car '()) ; no answer
