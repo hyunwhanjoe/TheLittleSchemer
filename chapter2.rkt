dfd15374abc6301f8d8031dbc8ce6448d754c028
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-07-02 16:20:23 +0900

    chapter2.rkt - add pg 16

--- chapter2.rkt
+++ chapter2.rkt
@@ -1,7 +1,22 @@
 #lang sicp
 
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l))(lat? (cdr l)))
+      (else #f))))
+
+;pg 15
 ;lat? list of atoms
 ;(lat? '(Jack Sprat could eat no chicken fat)) ;#t
 ;(lat? '((Jack) Sprat could eat no chicken fat)) ;#f
 ;(lat? '(Jack (Sprat could) eat no chicken fat)) ;#f
 ;(lat? '()) ;#t
+
+;pg 16
+(lat? '(bacon and eggs)) ;#t
