1bec65abd709472d3e25ee759fab3769121377b2
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-07-02 17:36:03 +0900

    chapter2.rkt - add pg 21

--- chapter2.rkt
+++ chapter2.rkt
@@ -4,11 +4,12 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
+; recursion
 (define lat?
   (lambda (l)
     (cond
       ((null? l) #t)
-      ((atom? (car l))(lat? (cdr l)))
+      ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
 
 ;pg 15
@@ -24,5 +25,10 @@
 ;(null? l) ;#f
 
 ;pg 19
-(define l '(bacon (and eggs)))
-(lat? l) ;#f
+;(define l '(bacon (and eggs)))
+;(lat? l) ;#f
+
+;pg 21
+(or (null? '()) (atom? '(d e f g))) 
+(or (null? '(a b c)) (null? '()))
+(or (null? '(a b c)))
