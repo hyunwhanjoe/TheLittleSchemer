86096f182bcfd96ed848da1730e1461f17caf569
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-07-07 17:49:41 +0900

    chapter3.rkt - add pg 33

---
+++ chapter3.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+;pg 33
+;(define lat '(lamb chops and mint jelly)) 
+;(rember 'mint lat) ;(lamb chops and jelly)
+;(define lat '(lamb chops and mint flavored mint jelly))
+;(rember 'mint lat) ;(lamb chops and flavored mint jelly)
+;(define lat '(bacon lettuce and tomato))
+;(rember 'toast lat) ;(bacon lettuce and tomato)
+;(define lat '(coffee cup tea cup and hick cup))
+;(rember 'cup lat) ;(coffee tea cup and hick cup)
+
