d5f8fb2ff63b9993ea6ab2e8babefd9147e7eacc
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-08-14 01:22:26 +0900

    chapter4.rkt - add pg 59 & 60

---
+++ chapter4.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+;pg 59
+;(atom? 14) ;#t
+;(add1 67) ;68
+;(sub1 5) ;4
+
+;pg 60
+;(sub1 0) ;-1
+;(zero? 0) ;#t
+;(zero? 1492) ;#f
+
+(define o+
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+; tail-recursive?
+;(define o+
+;  (lambda (n m)
+;    (cond
+;      ((zero? m) n)
+;      (else (o+ (add1 n) (sub1 m))))))
+
+(o+ 46 12) ;58
