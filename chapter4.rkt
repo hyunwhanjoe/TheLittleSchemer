200915d02de601201fb56a278b9170eb40941304
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-08-15 05:18:38 +0900

    chapter4.rkt - add pg 64 addtup

--- chapter4.rkt
+++ chapter4.rkt
@@ -22,11 +22,11 @@
 ;(zero? 0) ;#t
 ;(zero? 1492) ;#f
 
-;(define o+
-;  (lambda (n m)
-;    (cond
-;      ((zero? m) n)
-;      (else (add1 (o+ n (sub1 m)))))))
+(define o+
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
 ;
 ;; tail-recursive?
 ;;(define o+
@@ -38,12 +38,22 @@
 ;(o+ 46 12) ;58
 
 ;pg 61
-(define o-
-  (lambda (n m)
-    (cond
-      ((zero? m) n)
-      (else (sub1 (o- n (sub1 m)))))))
+;(define o-
+;  (lambda (n m)
+;    (cond
+;      ((zero? m) n)
+;      (else (sub1 (o- n (sub1 m)))))))
+;
+;(o- 14 3) ;11
+;(o- 17 9) ;8
+;(o- 18 25) ;-7
 
-(o- 14 3) ;11
-(o- 17 9) ;8
-(o- 18 25) ;-7
+;pg 64
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(addtup '(3 5 2 8)) ;18
+(addtup '(15 6 7 12 3)) ;43
