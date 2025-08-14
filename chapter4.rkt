1c147dbc03cbfae7cf72daa3eed5b6bdfff37bd3
hyunwhanjoe <hyunwhanjoe@gmail.com>
2025-08-15 05:12:52 +0900

    chapter4.rkt - add pg 61

--- chapter4.rkt
+++ chapter4.rkt
@@ -22,17 +22,28 @@
 ;(zero? 0) ;#t
 ;(zero? 1492) ;#f
 
-(define o+
-  (lambda (n m)
-    (cond
-      ((zero? m) n)
-      (else (add1 (o+ n (sub1 m)))))))
-
-; tail-recursive?
 ;(define o+
 ;  (lambda (n m)
 ;    (cond
 ;      ((zero? m) n)
-;      (else (o+ (add1 n) (sub1 m))))))
+;      (else (add1 (o+ n (sub1 m)))))))
+;
+;; tail-recursive?
+;;(define o+
+;;  (lambda (n m)
+;;    (cond
+;;      ((zero? m) n)
+;;      (else (o+ (add1 n) (sub1 m))))))
+;
+;(o+ 46 12) ;58
 
-(o+ 46 12) ;58
+;pg 61
+(define o-
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (sub1 (o- n (sub1 m)))))))
+
+(o- 14 3) ;11
+(o- 17 9) ;8
+(o- 18 25) ;-7
